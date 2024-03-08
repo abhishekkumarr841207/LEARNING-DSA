8304c5a0a9c128cede593d846a15021d2e4962ac
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-09 03:06:58 +0530

    add new file

---
+++ PRACTICE OF  C++/areaOfCircle.cpp
@@ -0,0 +1,14 @@
+//write a program to find the area of cicle.
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r,area;
+    cout<<"enter the value of radius =";
+    cin>>r;
+
+   area =3.14*(r*r);
+   cout<<"the area area of circle ="<<area<<endl;
+    
+   return 0;
+}
