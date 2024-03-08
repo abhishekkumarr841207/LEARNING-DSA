df8776c6e20f540c1749f54eeff0b31f3ab5a610
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-08 23:10:29 +0530

    add new file

---
+++ BASICS OF C++/operator1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num1=8;
+  int num2=4;
+  cout<<num1+num2<<endl; //12
+  cout<<num1-num2<<endl; // 4
+  cout<<num1*num2<<endl; //32
+  cout<<num1/num2<<endl; //2
+  cout<<num1%num2<<endl;// 0
+  return 0;
+
+}
