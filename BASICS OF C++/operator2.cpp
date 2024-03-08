d35e9d1d01575a457a1a945e033165ee48ab3ab4
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-08 23:40:47 +0530

    add new file

---
+++ BASICS OF C++/operator2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1=6;
+    int num2=3;
+    cout<<num1+num2<<endl; //9
+    cout<<num1*num2<<endl; //18
+    cout<<num1%num2<<endl; //
+    cout<<num1-num2<<endl; //
+    cout<<num1/num2<<endl; //
+
+
+    cout<<(num1==num2)<<endl; // false
+    cout<<(num1!=num2)<<endl; //true
+    cout<<(num1>=num2)<<endl; // true
+    return 0;
+}
