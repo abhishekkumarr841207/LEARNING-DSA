a7605a69ca0efe172da94f19eed1c558761780af
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-09 02:41:32 +0530

    add new file

---
+++ PRACTICE OF  C++/percentage Of Five Subject.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int D,C,E,M,H,Total
+    cout<<"Enter the marks of DSA =";
+    cin>>D
+    cout<<"Enter the marks of COMPUTER =";
+    cin>>C
+    cout<<"Enter the marks of ENGLISH =";
+    cin>>E
+    cout<<"Enter the marks of MATH =";
+    cin>>m
+    cout<<"Enter the marks of HINDI =";
+    cin>>H
+    Total=D+C+E+M+H
+    cout<<"total marks="<<total;
+
+}
