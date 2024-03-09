c2055d145026b27d0e5eca4236e1bbd3053516e4
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-10 00:55:18 +0530

    add new file

---
+++ CONDITION IN C++/evenOdd.cpp
@@ -0,0 +1,15 @@
+// Take positive integer input and tell if it is even or odd
+ #include<iostream>
+ using namespace std;
+ int main(){
+     int n=8;
+    if(n%4==0){
+        cout<<"This number is even"<<endl; 
+     }else{
+         cout<<"This number is odd"<<endl;
+     }
+     return 0;
+}
+
+// Take positive integer input and tell if it is odd
+
