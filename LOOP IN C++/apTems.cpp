258ac33605006446c97d5e1b8c1d579ae9038fc0
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-12 22:42:33 +0530

    add new file

---
+++ LOOP IN C++/apTems.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value"<<endl;
+   cin>>n;
+    for(int i=1;i<=n;i+=3){
+        cout<<"the arithematic prograssion is :"<<i<<endl;
+    }
+}
