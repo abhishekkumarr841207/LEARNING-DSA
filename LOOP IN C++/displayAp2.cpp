a14bffcdcafdf38a3ead9a9b5dcb41266b323b35
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-13 15:28:27 +0530

    add new file

---
+++ LOOP IN C++/displayAp2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value"<<endl;
+   cin>>n;
+    for(int i=4;i<=n;i+=3){
+        cout<<"the arithematic prograssion is :"<<i<<endl;
+    }
+    return 0;
+}
